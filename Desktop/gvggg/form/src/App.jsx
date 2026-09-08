01603f48239c7c4ef810f96fe0797b35c19ddeb4
gvgg <vg564802@gmail.com>
2026-09-08 10:47:31 +0530

    nav bar

--- Desktop/gvggg/form/src/App.jsx
+++ Desktop/gvggg/form/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
